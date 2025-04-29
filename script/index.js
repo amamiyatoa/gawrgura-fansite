9c8e4ee990351665a96f2e08be27395edc982da4
amamiyatoa <hrt.mr@icloud.com>
2025-04-29 13:03:23 +0900

    latest commit

--- script/index.js
+++ script/index.js
@@ -1,2 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    const imghover = document.getElementById('getimg');
+    imghover.addEventListener('mouseover', () => {
+        if(imghover.className != 'hoverIMG') {
+            imghover.classList.add('hoverIMG');
+            alert('hello');
+        }
+    });
+    imghover.addEventListener('mouseout', () => {
+        if(imghover.className == 'hoverIMG') {
+            imghover.classList.remove('hoverIMG');
+        }
+    });
 })
